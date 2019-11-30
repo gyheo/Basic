c545f32c4821d5f8c5c703c3537cc2f273eb7fec
gyheo <heo.geoyeo@gmail.com>
2019-11-30 18:39:35 +0900

    JavaScript Basic

---
+++ Javascript/inputLoop.js
@@ -0,0 +1,6 @@
+// gyheo 2019.11.30 Loops: while and for Task #6, Check the login
+let value = prompt("Enter a number greater than 100?", );
+
+while (value <= 100) {
+    value = prompt("Enter a number greater than 100?", );
+}
