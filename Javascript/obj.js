bfea3830340d70a3dfddddaac621306386cb6891
gyheo <heo.geoyeo@gmail.com>
2019-12-04 00:01:34 +0900

    object

--- Javascript/obj.js
+++ Javascript/obj.js
@@ -1,11 +1,11 @@
 // gyheo 2019.12.03 Object #1, Hello object
 
-let user;
+let user = {};
 
 user.name = 'John';
 user.surname = 'Smith';
 user.name = 'Pete';
-//delete user.name;
+delete user.name;
 
 console.log(user.name);
 console.log(user.surname);
