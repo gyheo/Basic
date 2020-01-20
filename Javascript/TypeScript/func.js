747e32ab182e2fcb0a272cb96e5f342f0ce5b11d
gyheo <heo.geoyeo@gmail.com>
2020-01-20 23:57:30 +0900

    Practice a Function

---
+++ Javascript/TypeScript/func.js
@@ -0,0 +1,7 @@
+function add(x, y) {
+    return x + y;
+}
+alert(add(10, 5));
+
+let addFunction = function (x, y) { return x + y; };
+alert(addFunction(10, -3));
---
+++ Javascript/TypeScript/func.ts
@@ -0,0 +1,9 @@
+function add(x: number, y: number) {
+    return x + y;
+}
+
+alert(add(10, 5));
+
+let addFunction = function(x: number, y: number): number { return x + y; };
+
+alert(addFunction(10, -3));
