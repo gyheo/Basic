3967d1c3b36be79eddd843d360e53948de10c966
gyheo <heo.geoyeo@gmail.com>
2020-01-22 23:52:50 +0900

    Optional Parameters

--- Javascript/TypeScript/func.ts
+++ Javascript/TypeScript/func.ts
@@ -7,3 +7,13 @@ alert(add(10, 5));
 let addFunction = function(x: number, y: number): number { return x + y; };
 
 alert(addFunction(10, -3));
+
+function addFunc(x: number, y: number, z?: number) {
+    if (z)
+        return x + y + z;
+    else
+        return x + y;
+}
+
+alert("2 + 4 + 8 = " + addFunc(2, 4, 8));
+alert("2 + 4 = " + addFunc(2, 4));
