e1e288110a1f68183021eadc37f8a3a8521f11eb
gyheo <heo.geoyeo@gmail.com>
2020-01-26 13:57:29 +0900

    Practice a Swift Tour

--- Swift/Swift.playground/Contents.swift
+++ Swift/Swift.playground/Contents.swift
@@ -9,3 +9,25 @@ for i in 0..<100 {
     sin(Double(i) / 2)
     // cos(Double(i) / 2)
 }
+
+let singers = ["Anne", "Ed", "Chris", "Tom"]
+for singer in singers {
+    print("Hello, \(singer)!")
+}
+
+let interestingNumbers = [
+    "Prime": [2, 3, 5, 7, 11, 13, 17],
+    "Fibonacci": [1, 1, 2, 3, 5, 8, 13],
+    "Square": [1, 4, 9, 16, 25, 36],
+]
+
+var largest = 0
+for (kind, numbers) in interestingNumbers {
+    for number in numbers {
+        if number > largest {
+            largest = number
+        }
+    }
+}
+
+print(largest)
