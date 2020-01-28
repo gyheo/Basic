683adab03ed0afee6961df4bde0a3d20a7a71038
gyheo <heo.geoyeo@gmail.com>
2020-01-28 23:59:05 +0900

    First iOS App commit

---
+++ Swift/FirstApp/FirstApp/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  FirstApp
+//
+//  Created by 허건영 on 28/01/2020.
+//  Copyright © 2020 gyheo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
