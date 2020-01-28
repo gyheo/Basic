60dab6e6d779e47e9d960f07c74a4cfb4e2c092c
gyheo <heo.geoyeo@gmail.com>
2020-01-29 00:35:17 +0900

    Making a Single App

---
+++ Swift/gyheo/gyheo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gyheo
+//
+//  Created by 허건영 on 29/01/2020.
+//  Copyright © 2020 geo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
