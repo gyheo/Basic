9da2a87fad38a3e61b47ad090f0836a5863780cc
gyheo <heo.geoyeo@gmail.com>
2020-01-30 23:53:19 +0900

    Practice a backgroundColor

---
+++ Javascript/TypeScript/bgBtn.js
@@ -0,0 +1,6 @@
+const bgBtn = document.querySelector("#bgBtn");
+
+bgBtn.onclick = function () {
+    const color = "rgb(" + (Math.random() * 255 + 1) + "," + (Math.random() * 255 + 1) + "," + (Math.random() * 255 + 1) + ")";
+    document.body.style.backgroundColor = color;
+};
