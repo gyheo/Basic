9b2eab281aa5b1898b4eef5951667279ec37d145
gyheo <heo.geoyeo@gmail.com>
2020-02-03 23:57:52 +0900

    Practice a Iterators and Generators

---
+++ Javascript/TypeScript/Iterable.ts
@@ -0,0 +1,16 @@
+let someArray = [1, 'string', true];
+
+for (let entry of someArray) {
+    alert(entry);
+}
+
+let pets = new Set(["Cat", "Dog", "Horse"]);
+pets["species"] = "mammals";
+
+for(let pet in pets) {
+    console.log(pet);
+}
+
+for(let pet of pets) {
+    console.log(pet);
+}
