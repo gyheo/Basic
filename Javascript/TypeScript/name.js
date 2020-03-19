e081107f9280334b008f503ae509909c0c992796
gyheo <heo.geoyeo@gmail.com>
2020-03-19 22:37:14 +0900

    Practice a eventlistener

--- Javascript/TypeScript/name.js
+++ Javascript/TypeScript/name.js
@@ -1,6 +1,9 @@
 const btn = document.querySelector("#btn");
-
-btn.onclick = function () {
-    let name = prompt("What is your name?", "");
-    alert('Hello! ' + name + ' nice to meet you!');
-}
+// btn.onclick = function() {
+//     let name: string = prompt("What is your name?", "");
+//     alert('Hello! ' + name + ' nice to meet you!');
+// }
+btn.addEventListener('click', function () {
+    let name = prompt("What is your name", "");
+    alert(`Hello ${name} nice to meet you!`);
+});
--- Javascript/TypeScript/name.ts
+++ Javascript/TypeScript/name.ts
@@ -1,6 +1,12 @@
 const btn = document.querySelector("#btn");
 
-btn.onclick = function() {
-    let name: string = prompt("What is your name?", "");
-    alert('Hello! ' + name + ' nice to meet you!');
-}
+// btn.onclick = function() {
+//     let name: string = prompt("What is your name?", "");
+//     alert('Hello! ' + name + ' nice to meet you!');
+// }
+
+// Practice a eventListener
+btn.addEventListener('click', function() {
+    let name: string = prompt("What is your name", "");
+    alert(`Hello ${name} nice to meet you!`);
+})
