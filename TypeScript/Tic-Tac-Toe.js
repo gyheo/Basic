f37b0b89cfc7df42878366c0b5f06df23a9fe534
gyheo <heo.geoyeo@gmail.com>
2020-04-30 12:57:04 +0900

    select the player & input the X, O to the table

--- TypeScript/Tic-Tac-Toe.js
+++ TypeScript/Tic-Tac-Toe.js
@@ -2,13 +2,45 @@ const table = document.querySelector('table');
 let tableRows = document.querySelectorAll('tr');
 let tableArrays = [];
 function init() {
+    // const form = document.querySelector("form");
+    let XO = document.getElementsByName("XO");
+    let turn = true;
+    let player = XO[0].value; // player 처음은 X, computer는 O
+    let computer = XO[1].value;
+    for (let i = 0; i < 2; i++) {
+        XO[i].addEventListener('change', function (event) {
+            // 말 바꾸기
+            if (XO[0].checked) {
+                player = XO[0].value;
+                computer = XO[1].value;
+            }
+            else {
+                player = XO[1].value;
+                computer = XO[0].value;
+            }
+        });
+    }
     for (let i = 0; i < tableRows.length; i++) {
         tableArrays.push(tableRows[i].querySelectorAll('td'));
     }
     for (let i = 0; i < tableRows.length; i++) {
         for (let j = 0; j < tableArrays.length; j++) {
             tableArrays[i][j].addEventListener('click', function () {
-                console.log(`${i}, ${j}`);
+                // 행, 열 표시
+                // console.log(`${i}, ${j}`);
+                XO[0].disabled = true;
+                XO[1].disabled = true;
+                // 빈 칸일 경우에만 동작
+                if (tableArrays[i][j].textContent === "") {
+                    if (turn) {
+                        tableArrays[i][j].textContent = player;
+                        turn = false;
+                    }
+                    else {
+                        tableArrays[i][j].textContent = computer;
+                        turn = true;
+                    }
+                }
             });
         }
     }
--- TypeScript/Tic-Tac-Toe.ts
+++ TypeScript/Tic-Tac-Toe.ts
@@ -3,7 +3,25 @@ let tableRows = document.querySelectorAll('tr');
 let tableArrays = [];
 
 function init() {
-    
+    // const form = document.querySelector("form");
+    let XO = document.getElementsByName("XO");
+    let turn:boolean = true;
+    let player: string = XO[0].value; // player 처음은 X, computer는 O
+    let computer: string = XO[1].value;
+
+    for(let i: number = 0; i < 2; i++) {
+        XO[i].addEventListener('change', function(event) {
+            // 말 바꾸기
+            if(XO[0].checked) {
+                player = XO[0].value;
+                computer = XO[1].value
+            } else {
+                player = XO[1].value;
+                computer = XO[0].value
+            }
+        })
+    }
+
     for(let i:number = 0; i < tableRows.length; i++) {
         tableArrays.push(tableRows[i].querySelectorAll('td'));
     }
@@ -11,7 +29,22 @@ function init() {
     for(let i:number = 0; i < tableRows.length; i++) {
         for(let j:number = 0; j < tableArrays.length; j++){
             tableArrays[i][j].addEventListener('click', function(){
-                console.log(`${i}, ${j}`);
+                // 행, 열 표시
+                // console.log(`${i}, ${j}`);
+                XO[0].disabled = true;
+                XO[1].disabled = true;
+
+                // 빈 칸일 경우에만 X, O 입력 허용
+                if(tableArrays[i][j].textContent === "") {
+                    if(turn){
+                        tableArrays[i][j].textContent = player;
+                        turn = false;
+                    }
+                    else{
+                        tableArrays[i][j].textContent = computer;
+                        turn = true;
+                    }
+                }
             });
         }
     }
