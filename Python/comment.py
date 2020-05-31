b966afd9c127b39a1dbc6bbe4a58addbbbeba253
gyheo <heo.geoyeo@gmail.com>
2020-05-31 23:17:34 +0900

    Practice print and comments

---
+++ Python/comment.py
@@ -0,0 +1,14 @@
+# We adds comments to our code using #
+# This is a comment in my code it does nothing
+
+# Lines that starts with # are not executed
+
+# Comment calls to other programs to explain their purpose
+
+# Comments document your code so you and other programmers can understand the code
+
+# Using double quotes for this string because
+# the string itself contains a single quote
+print("It's a small world after all")
+
+# Commenting lines of code can help you debug and figure out which line of code is causing an error
---
+++ Python/hi.py
@@ -0,0 +1,24 @@
+# print displays output to your console
+# Enclosing strings in the single or double quotes
+print("hi")
+print('hi')
+
+# Getting information from the user
+
+# name = input('Please enter your name : ')
+name = raw_input('Please enter your name : ')
+print(name)
+
+# Printing blank lines can improve readability
+
+print("Hello")
+print()
+print("Did yout see blank line?")
+print('Blank line \nin the middle of the string')
+
+# Debugging with print
+print('Adding numbers')
+x = 123 + 123
+print("Performing division")
+y = x / 0
+print("Math complete")
