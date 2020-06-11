3e4c3563c6346c40146163e3d23096bf0036307d
gyheo <heo.geoyeo@gmail.com>
2020-06-11 23:59:51 +0900

    Practice dictionary and list

---
+++ Python/Collections.py
@@ -0,0 +1,23 @@
+# Lists are collections of items
+
+# Arrays are also collections of items
+
+# What's the difference 
+# Arrays Numeric data types
+# Lists Store anything
+
+# Retrieving ranges
+heogeoyeong = {}
+heogeoyeong['first'] = "Heo"
+heogeoyeong['last'] = "GeonYeong"
+
+Tom = {}
+Tom['first'] = "Tom"
+Tom['last'] = "Boy"
+
+print(heogeoyeong)
+print(Tom)
+
+people = []
+people.append(heogeoyeong)
+people.append(Tom)
