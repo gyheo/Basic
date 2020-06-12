05722aeab038f39793b856a20a33f62cbac584ee
gyheo <heo.geoyeo@gmail.com>
2020-06-12 23:54:19 +0900

    Practice Loops with condition

---
+++ Python/Loops.py
@@ -0,0 +1,16 @@
+# Loop through a collection
+# for language in ["HTML", "CSS", "JavaScript"]:
+web_language = ["HTML", "CSS", "JavaScript"]
+for language in web_language:
+    print(language)
+
+# Looping a number of times
+for index in range(1, 11):
+    print(index)
+
+# Looping with a condition
+language = ["Python", "Ruby", "Java"]
+i = 0
+while i < len(language):
+    print(language[i])
+    i = i + 1
