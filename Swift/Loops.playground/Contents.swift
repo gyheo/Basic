43f81536d101808b97801b209171b876674f184b
gyheo <gyheo@outlook.com>
2020-08-10 23:48:57 +0900

    Practice for loops

---
+++ Swift/Loops.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var str = "Hello, playground"
+
+print("Hello")
+
+var sum = 0
+for counter in 1...5 {
+    // print("counter number is ",counter)
+    
+    // var sum = 0
+    // sum += counter
+    sum = sum + counter
+}
+print(sum)
