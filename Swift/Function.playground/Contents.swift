26ad437fa5989bec8232b87c8225ed562b61b4cf
gyheo <gyheo@outlook.com>
2020-08-12 23:54:12 +0900

    Practice func and variable scope

---
+++ Swift/Function.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+var str = "Hello, playground"
+
+func addTwoNumbers () {
+    let x = 2
+    let y = 3
+    let z = x + y
+    
+    print(z)
+}
+
+// local variable declared in the scope
+// print(x)
+
+addTwoNumbers()
+
+func subtractTwoNumbers() {
+    let p = 4
+    let q = 3
+    let r = p - q
+    print(r)
+}
+
+subtractTwoNumbers()
