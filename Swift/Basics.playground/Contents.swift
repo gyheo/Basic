668670ea1f5cd98b7bfa4d4f65ebcdc2541e5b80
gyheo <gyheo@outlook.com>
2020-08-23 17:18:39 +0900

    Practice the operators

--- Swift/Basics.playground/Contents.swift
+++ Swift/Basics.playground/Contents.swift
@@ -59,3 +59,87 @@ print("The statusMessage is \(statusMessage)")
 let http200Status = (statusCode: 200, description: "OK")
 print("The status code is \(http200Status.statusCode)")
 print("The statusMessage is \(http200Status.description)")
+
+let b = 1
+var a = 14
+
+// = Assignment Operator
+a = b
+print(a)
+
+1 + 1 + 1
+4 - 2
+1 * 6
+10.0 / 2.5
+
+// Remainder Operator
+13 % 4
+
+print(3 == 3)
+print(3 != 4)
+print(3 > 2)
+print(1 < 3)
+print(3 >= 1)
+print(3 <= 1)
+
+var languageName:String = "Swift"
+
+// languageName = "Python"
+
+if languageName == "Swift" {
+    print("Hello iOS World with \(languageName)")
+}
+
+(1, "One") < (2, "Two")
+(3, "Three") < (4, "Four")
+(1, "One") == (1, "One")
+(1, "One") == (0, "Zero")
+
+// print(true < false)
+print(800.0 - 784.1)
+
+let contentHeight = 40
+let hasHeader = true
+let rowHeight = contentHeight + (hasHeader ? 50 : 20)
+print(rowHeight)
+
+let ITcompanies = ["Apple", "Google", "Microsoft", "MI"]
+
+for index in 0...100 {
+    print(index)
+}
+
+for i in 0..<ITcompanies.count {
+    print(ITcompanies[i])
+}
+
+for IT in ITcompanies[2...] {
+    print(IT)
+}
+
+for IT in ITcompanies[..<2] {
+    print(IT)
+}
+
+let range = ...50
+range.contains(-10)
+range.contains(0)
+range.contains(50)
+range.contains(51)
+
+// Logical Operators
+let allowedEntry = false
+
+if !allowedEntry {
+    print("ACCESS DENIED")
+}
+
+let numberPassword = true
+let fingerPrint = false
+
+//if fingerPrint && numberPassword {
+if fingerPrint || numberPassword {
+    print("ACCESS OK")
+} else {
+    print("ACCESS DENIED")
+}
