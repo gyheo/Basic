b10a360b0f1c4b58d354bfed4e429f1af351bfac
gyheo <gyheo@outlook.com>
2020-09-01 23:50:10 +0900

    Practice insert and remove in the array

--- Swift/CollectionTypes.playground/Contents.swift
+++ Swift/CollectionTypes.playground/Contents.swift
@@ -16,3 +16,26 @@ var sixDoubles = threeDoubles + anotherThreeDoubles
 
 var shoppingList: [String] = ["Eggs", "Cheese"]
 let shoppingList2 = ["Apple", "Coke"]
+
+shoppingList.append("Corn")
+//shoppingList.append(5)
+
+shoppingList += ["Tomato"]
+
+print("shoppingList total size : \(shoppingList.count)")
+
+if !shoppingList.isEmpty {
+    for item in shoppingList {
+        print(item)
+    }
+} else {
+    print("There is nothing in a shoppingList")
+}
+
+print(shoppingList[2...3])
+
+shoppingList.insert("Maple Syrup", at: 2)
+shoppingList
+
+var anotherList = shoppingList.removeLast()
+print(anotherList)
