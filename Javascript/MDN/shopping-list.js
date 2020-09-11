c0f2f6ce6bacd0a64a9921acdcfb9faa7199b719
gyheo <gyheo@outlook.com>
2020-09-11 23:57:55 +0900

    Using the Prettier and Adding the delete button

--- Javascript/MDN/shopping-list.js
+++ Javascript/MDN/shopping-list.js
@@ -1,12 +1,30 @@
 let button = document.getElementsByTagName("button");
-let item = document.getElementById("item");
-let shoppingList = document.getElementById("shoppingList");
+let item = document.querySelector("#item");
+let shoppingList = document.querySelector("#shoppingList");
 
-button[0].addEventListener("click", function(){
-    let li = document.createElement('li');
+function init() {
+  button[0].addEventListener("click", function () {
+    var num = 0;
+    let li = document.createElement("li");
+    let deleteButton = document.createElement("button");
+
+    deleteButton.innerHTML = "Delete";
+    deleteButton.addEventListener("click", function () {
+      deleteButton.parentElement.remove();
+    });
+
     li.innerHTML = item.value;
+    li.appendChild(deleteButton);
+
+    if (item.value.trim() == "") {
+      return;
+    }
+
     shoppingList.appendChild(li);
 
     item.value = "";
     item.focus();
-});
+  });
+}
+
+init();
