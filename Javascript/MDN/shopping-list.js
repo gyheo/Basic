050e8095cb4ac5b71a8376cd0f5915f02f75c581
gyheo <gyheo@outlook.com>
2020-09-12 23:40:57 +0900

    Add click and keyup eventlistener

--- Javascript/MDN/shopping-list.js
+++ Javascript/MDN/shopping-list.js
@@ -1,30 +1,39 @@
-let button = document.getElementsByTagName("button");
+let button = document.querySelector("button");
 let item = document.querySelector("#item");
 let shoppingList = document.querySelector("#shoppingList");
 
 function init() {
-  button[0].addEventListener("click", function () {
-    var num = 0;
-    let li = document.createElement("li");
-    let deleteButton = document.createElement("button");
+  item.focus();
 
-    deleteButton.innerHTML = "Delete";
-    deleteButton.addEventListener("click", function () {
-      deleteButton.parentElement.remove();
-    });
-
-    li.innerHTML = item.value;
-    li.appendChild(deleteButton);
-
-    if (item.value.trim() == "") {
-      return;
+  item.addEventListener("keyup", function (event) {
+    if (event.keyCode == 13) {
+      AddItems();
     }
-
-    shoppingList.appendChild(li);
-
-    item.value = "";
-    item.focus();
   });
+
+  button.addEventListener("click", AddItems);
+}
+
+function AddItems() {
+  let li = document.createElement("li");
+  let deleteButton = document.createElement("button");
+
+  deleteButton.innerHTML = "Delete";
+  deleteButton.addEventListener("click", function () {
+    deleteButton.parentElement.remove();
+  });
+
+  li.innerHTML = item.value;
+  li.appendChild(deleteButton);
+
+  if (item.value.trim() == "") {
+    return;
+  }
+
+  shoppingList.appendChild(li);
+
+  item.value = "";
+  item.focus();
 }
 
 init();
