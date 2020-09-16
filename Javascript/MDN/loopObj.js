93cec5db184a3c42ba28e20a595b337a8203f4fa
gyheo <gyheo@outlook.com>
2020-09-16 23:44:12 +0900

    Drawing the circle using setInterval

--- Javascript/MDN/loopObj.js
+++ Javascript/MDN/loopObj.js
@@ -1,11 +1,23 @@
-function init() {
-  let canvas = document.getElementById("canvas");
-  var ctx = canvas.getContext("2d");
-  ctx.strokeStyle = "rgba(0, 0, 200, 0.5)";
+window.addEventListener("onload", init());
 
-  for (let i = 1; i <= 10; i++) {
-    ctx.beginPath();
-    ctx.arc(250, 250, 10 * i, 0, Math.PI * 2, false);
-    ctx.stroke();
-  }
+function init() {
+  setInterval(drawCircle, 1000);
+}
+
+function drawCircle() {
+  let today = new Date();
+  let seconds = today.getSeconds();
+
+  let canvas = document.getElementById("canvas");
+  let ctx = canvas.getContext("2d");
+  let radius = 100;
+
+  ctx.strokeStyle = "rgba(0, 110, 200, 0.5)";
+
+  let circleX = 250 + Math.cos((Math.PI / 8) * seconds) * radius;
+  let circleY = 250 + Math.sin((Math.PI / 8) * seconds) * radius;
+  // ctx.arc(250, 250, 10 * i, 0, Math.PI * 2, false);
+  ctx.beginPath();
+  ctx.arc(circleX, circleY, radius, 0, Math.PI * 2, true);
+  ctx.stroke();
 }
