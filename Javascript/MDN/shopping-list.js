bf9cd2240d6404d75f24826a1ef96d9532de065a
gyheo <gyheo@outlook.com>
2020-09-24 23:58:45 +0900

    Adding a localStorage to the shopping-list (draft)

--- Javascript/MDN/shopping-list.js
+++ Javascript/MDN/shopping-list.js
@@ -1,9 +1,30 @@
 let button = document.querySelector("button");
 let item = document.querySelector("#item");
 let shoppingList = document.querySelector("#shoppingList");
+let index = 0;
 
 function init() {
   item.focus();
+
+  if (localStorage.length > 0) {
+    for (let key = 0; key < localStorage.length; key++) {
+      let li = document.createElement("li");
+      li.setAttribute("id", key);
+
+      let deleteButton = document.createElement("button");
+
+      deleteButton.innerHTML = "Delete";
+      deleteButton.addEventListener("click", function () {
+        localStorage.removeItem(key.toString());
+        deleteButton.parentElement.remove();
+      });
+
+      li.innerHTML = localStorage.getItem(key);
+      li.appendChild(deleteButton);
+
+      shoppingList.appendChild(li);
+    }
+  }
 
   item.addEventListener("keyup", function (event) {
     if (event.keyCode == 13) {
@@ -16,10 +37,14 @@ function init() {
 
 function AddItems() {
   let li = document.createElement("li");
+  li.setAttribute("id", index);
+  index = index + 1;
   let deleteButton = document.createElement("button");
 
   deleteButton.innerHTML = "Delete";
   deleteButton.addEventListener("click", function () {
+    localStorage.removeItem(index.toString());
+    index = index - 1;
     deleteButton.parentElement.remove();
   });
 
@@ -31,6 +56,7 @@ function AddItems() {
   }
 
   shoppingList.appendChild(li);
+  localStorage.setItem(index.toString(), item.value);
 
   item.value = "";
   item.focus();
