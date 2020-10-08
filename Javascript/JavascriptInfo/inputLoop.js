361feedd75cc896d69ff9cece515f57a8bd7b667
gyheo <gyheo@outlook.com>
2020-10-08 23:46:25 +0900

    Remove the JavaScriptInfo directory

--- Javascript/JavascriptInfo/inputLoop.js
+++
@@ -1,6 +0,0 @@
-// gyheo 2019.11.30 Loops: while and for Task #6, Check the login
-let value = prompt("Enter a number greater than 100?", );
-
-while (value <= 100) {
-    value = prompt("Enter a number greater than 100?", );
-}
--- Javascript/JavascriptInfo/number.js
+++
@@ -1,8 +0,0 @@
-// gyheo 2019.12.10 Numbers Task #2, A random Integer min to max
-function randomInteger(min, max){
-    return Math.floor(min + Math.random() * (max + 1 - min));
-}
-
-alert(randomInteger(1, 5));
-alert(randomInteger(1, 5));
-alert(randomInteger(1, 5));
